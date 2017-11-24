5fdaffa3a47439a80d1f768fc0039e7b56550631
Michael Torrie <torriem@gmail.com>
2017-11-23 22:35:44 -0700

    CModuleComm updated to AOG bf871446b08f86579c197822463810a8011970b5
    Unfortunately we can't refer to main form in this class due to circular
    references that we want to avoid, so the caller will have to call
    rateRelayControlOutToPort and autoSteerDataOutToPort on the main form
    after calling the resetAllModuleCommValues() method.
    
    Maybe in the future we could Qt events to allow resetAllModuleCommValues
    to trigger calls to those functions.

--- classes/cmodulecomm.cpp
+++ classes/cmodulecomm.cpp
@@ -13,11 +13,27 @@
 
 CModuleComm::CModuleComm()
 {
-    serialRecvAutoSteerStr = ",";
-    serialRecvRelayStr = ",";
+    serialRecvAutoSteerStr = "Oops";
+    serialRecvRelayRateStr = "Oops";
 
-    //control all relays based on byte value, 1 means on, 0 means off
-    relaySectionControl[0] = 0;
+    //WorkSwitch logic
+    isWorkSwitchEnabled = false;
+    isWorkSwitchOn = false;
+
+    //does a low, grounded out, mean on
+    isWorkSwitchActiveLow = true;
+
+}
+
+void CModuleComm::resetAllModuleCommValues()
+{
+    //set up relayRate array
+    relayRateControl[rcHeaderHi] = 127; //32762
+    relayRateControl[rcHeaderLo] = 250;
+    relayRateControl[rcSectionControlByte] = 0;
+    relayRateControl[rcRateSetPointHi] = 0;
+    relayRateControl[rcRateSetPointLo] = 0;
+    relayRateControl[rcSpeedXFour] = 0;
 
     //prefill the autosteer data
     autoSteerData[sdHeaderHi] = 127; //32766
@@ -29,18 +45,10 @@ CModuleComm::CModuleComm()
     autoSteerData[sdSteerAngleHi] = (125); //32020
     autoSteerData[sdSteerAngleLo] = 20;
 
-    //WorkSwitch logic
-    isWorkSwitchEnabled = false;
-    isWorkSwitchOn = false;
-
-    //does a low, grounded out, mean on
-    isWorkSwitchActiveLow = true;
-
     //prefill the autosteer settings from settings xml
+    AOGSettings s;
     autoSteerSettings[ssHeaderHi] = 127;
     autoSteerSettings[ssHeaderLo] = 252; //32764 as header
-
-    AOGSettings s;
     autoSteerSettings[ssKp] = s.value("autosteer/Kp", DEFAULT_SETAS_KP).toInt();
     autoSteerSettings[ssKi] = s.value("autosteer/Ki", DEFAULT_SETAS_KI).toInt();
     autoSteerSettings[ssKd] = s.value("autosteer/Kd", DEFAULT_SETAS_KD).toInt();
@@ -49,4 +57,13 @@ CModuleComm::CModuleComm()
     autoSteerSettings[ssMinPWM] = s.value("autosteer/minSteerPWM", DEFAULT_SETAS_MINSTEERPWM).toInt();
     autoSteerSettings[ssMaxIntegral] = s.value("autosteer/maxIntegral", DEFAULT_SETAS_MAXINTEGRAL).toInt();
     autoSteerSettings[ssCountsPerDegree] = s.value("autosteer/countsPerDegree", DEFAULT_SETAS_COUNTSPERDEGREE).toInt();
+
+    //For now, caller must call these routines as we don't know
+    //about the main form here, in order to avoid circular dependencies
+
+    //spit out to rate/relay module
+    //mf.RateRelayControlOutToPort();
+
+    //out serial to autosteer module  //indivdual classes load the distance and heading deltas
+    //mf.AutoSteerDataOutToPort();
 }
--- classes/cmodulecomm.h
+++ classes/cmodulecomm.h
@@ -8,42 +8,46 @@
 class CModuleComm
 {
 public:
-    //properties for relay control of sections and input lines, 8 bit bytes
-    static const int numRelayControls = 1;
-    uchar relaySectionControl[numRelayControls];
+    //receive string for RateRelay
+    QString serialRecvRelayRateStr;
 
-    QString serialRecvRelayStr = "";
+    //properties for rate control of sections and input lines, 8 bit bytes
+    static const int numRelayRateControls = 6;
+    uchar relayRateControl[numRelayRateControls];
+    enum relayRatecontrolItemsIndex {
+        rcHeaderHi = 0, rcHeaderLo = 1, rcSectionControlByte = 2, rcSpeedXFour = 3,
+        rcRateSetPointHi = 4, rcRateSetPointLo = 5
+    };
 
-    //public double angularVelocity = 0;
-    //public double imuHeading = 0;
 
+    //recv for the AutoSteer module
+    QString serialRecvAutoSteerStr;
     static const int numSteerDataItems = 8;
-    enum steerDataItemsIndex { sdHeaderHi = 0, sdHeaderLo=1,
-                               sdRelay=2, sdSpeed=3, sdDistanceHi=4,
-                               sdDistanceLo=5, sdSteerAngleHi=6,
-                               sdSteerAngleLo=7 };
+    uchar autoSteerData[numSteerDataItems];
+    enum steerDataItemsIndex {
+        sdHeaderHi = 0, sdHeaderLo=1, sdRelay=2, sdSpeed=3, sdDistanceHi=4,
+        sdDistanceLo=5, sdSteerAngleHi=6, sdSteerAngleLo=7
+    };
 
     static const int numSteerSettingItems = 10;
+    uchar autoSteerSettings[numSteerSettingItems];
     enum steerSettingItemsIndex { ssHeaderHi = 0, ssHeaderLo = 1,
                                   ssKp = 2, ssKi = 3, ssKd = 4,
                                   ssKo = 5, ssSteerOffset = 6, ssMinPWM = 7,
                                   ssMaxIntegral = 8, ssCountsPerDegree = 9 };
 
-
-    //control for the auto steer module
-
-    uchar autoSteerData[numSteerDataItems];
-    uchar autoSteerSettings[numSteerSettingItems];
-    QString serialRecvAutoSteerStr = "";
-
-    //from autosteer module the a/d conversion of inclinometer
-    int rollRaw = 0;
-
     //for the workswitch
     bool isWorkSwitchOn=false, isWorkSwitchActiveLow=false, isWorkSwitchEnabled=false;
     int workSwitchValue = 0;
 
+    //from autosteer module the a/d conversion of inclinometer
+    //imu and roll inclinometer
+    int steerSwitchValue, gyroHeading=9999, prevGyroHeading = 9999;
+    int rollRaw = 9999; //inclinometer ?
+
+
     CModuleComm();
+    void resetAllModuleCommValues();
 };
 
 #endif // CMODULECOMM_H
