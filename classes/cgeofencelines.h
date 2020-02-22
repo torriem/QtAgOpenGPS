9e4dab6f4b76c359a470f5a60fd33d0946bf0b16
Michael Torrie <torriem@gmail.com>
2020-02-22 15:15:15 -0700

    forgot to add CGeoFenceLines files

--- classes/cgeofencelines.h
+++ classes/cgeofencelines.h
@@ -1,11 +1,31 @@
 #ifndef CGEOFENCELINES_H
 #define CGEOFENCELINES_H
 
+#include <QVector>
+#include "vec2.h"
+#include "vec3.h"
+
+class QOpenGLFunctions;
+class QMatrix4x4;
+
 
 class CGeoFenceLines
 {
 public:
+    QVector<Vec2> geoFenceLine;
+    QVector<Vec2> calcList;
+
+
     CGeoFenceLines();
+    void reset();
+    bool isPointInGeoFenceArea(Vec3 testPointv2);
+    bool isPointInGeoFenceArea(Vec2 testPointv2);
+    void drawGeoFenceLine(QOpenGLFunctions *gl, const QMatrix4x4 &mvp);
+    void fixGeoFenceLine(double totalHeadWidth, QVector<Vec3> curBnd, double spacing);
+    void preCalcTurnLines();
+
+
+
 };
 
 #endif // CGEOFENCELINES_H
