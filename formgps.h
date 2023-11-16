476212d3cb182fffcd017fc3f30145fea564b34d
Davidwedel <davidwedel06@gmail.com>
2023-11-15 18:19:54 -0600

    Actually removed said buttons, instead of just commenting

--- formgps.h
+++ formgps.h
@@ -81,7 +81,6 @@ public:
     QObject *btnnavigationSettings;
     QObject *btnSettings;
     QObject *btnTools;
-//    QObject *btnPerimeter; //remove this someone plz
     QObject *btnFieldMenu;
     QObject *btnAgIO;
     QObject *btnautoSteerConf;
@@ -105,15 +104,6 @@ public:
     QObject *btnContourPriority;
     //other
     QObject *btnFlag;
-
-    //menu button and icon palette -- do we need to keep all of these?
- /*   QObject *btnMenuDrawer;
-    QObject *btnJob;
-    QObject *btnBoundaryMenu;
-    QObject *btnComm;
-    QObject *btnUnits;
-    QObject *btnFileExplorer;
-    QObject *btnAutoSteerConfig;*/
 
     //area context menu
     QObject *contextArea;
