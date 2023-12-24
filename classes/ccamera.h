444c37cadd0332b3e5cb775a5a5f5c1adc7e3cff
Michael Torrie <torriem@gmail.com>
2023-12-23 23:07:10 -0700

    updated CCamera class

--- classes/ccamera.h
+++ classes/ccamera.h
@@ -13,10 +13,13 @@ private:
 public:
     double camYaw = 0;
 
+    double camPitch;
     double offset = 0;
+    double panX = 0, panY = 0;
     double camSetDistance = -75; //put in setttings?
-    //double camDelta = 0;
+
     double gridZoom;
+
     double zoomValue = 15;
     double previousZoom = 25;
 
@@ -24,8 +27,10 @@ public:
 
     bool camFollowing;
     int camMode = 0;
+    double camSmoothFactor;
 
     CCamera();
+    void loadSettings();
 
     void setWorldCam(QMatrix4x4 &modelview, double _fixPosX, double _fixPosY, double _fixHeading);
 
