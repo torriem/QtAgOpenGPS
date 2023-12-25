ebfbcd7eb0b6f9a2d47542209b448dc67d83ef09
Michael Torrie <torriem@gmail.com>
2023-12-24 22:13:29 -0700

    updated CWorldGrid.  I think I got the GL buffers right.

--- classes/cworldgrid.h
+++ classes/cworldgrid.h
@@ -9,6 +9,7 @@
 class QOpenGLContext;
 class QOpenGLFunctions;
 class QOpenGLShaderProgram;
+class CCamera;
 
 class CWorldGrid
 {
@@ -20,28 +21,52 @@ private:
 
     bool fieldBufferCurrent = false;
     double lastGridZoom = -1;
+    double lastCount = -1;
+    double lastNorthingMax = 0;
+    double lastNorthingMin = 0;
+    double lastEastingMax = 0;
+    double lastEastingMin = 0;
+
 
     inline void invalidateBuffers() {
+        if ((lastEastingMax != eastingMax) ||
+            (lastEastingMin != eastingMin) ||
+            (lastNorthingMax != northingMax) ||
+            (lastNorthingMin != northingMin) ||
+            (lastCount != Count) ) {
         lastGridZoom = -1;
         fieldBufferCurrent = false;
+        lastCount = -1;
+        }
     }
 
 public:
-    //Z (Y)
+    //Y
     double northingMax = 0;
     double northingMin = 0;
 
+    //X
     double eastingMax = 0;
     double eastingMin = 0;
 
-    double texZoomE = 20;
-    double texZoomN = 20;
+    //Y
+    double northingMaxGeo;
+
+    double northingMinGeo;
+
+    //X
+    double eastingMaxGeo;
+
+    double eastingMinGeo;
+
+    double GridSize = 4000;
+    double Count = 30;
+    bool isGeoMap = false;
 
     CWorldGrid();
     ~CWorldGrid();
-    void drawFieldSurface(QOpenGLFunctions *gl, const QMatrix4x4 &mvp, QColor fieldColor);
+    void drawFieldSurface(QOpenGLFunctions *gl, const QMatrix4x4 &mvp, bool isTextureOn, QColor fieldColor, CCamera &camera);
     void drawWorldGrid(QOpenGLFunctions *gl, const QMatrix4x4 &mvp, double _gridZoom, QColor gridColor);
-    void createWorldGrid(double northing, double easting);
     void checkZoomWorldGrid(double northing, double easting);
     void destroyGLBuffers();
 
