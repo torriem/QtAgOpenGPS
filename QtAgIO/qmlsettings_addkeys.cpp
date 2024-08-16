9114f23da7cbf6fc2bd733f609a7327c912fb779
davidwedel <DavidWedel06@gmail.com>
2024-08-16 08:30:27 -0500

    Had to change a few data types in here to work correctly. I also have some annoying errors in qmlsettings.cpp. Depricated stuff that worked fine in AOG side but don't work here for  some reason. So this branch won't compile yet.

--- QtAgIO/qmlsettings_addkeys.cpp
+++ QtAgIO/qmlsettings_addkeys.cpp
@@ -1,19 +1,19 @@
 #include "qmlsettings.h"
 
 void QMLSettings::setupKeys() {
-	addKey(QString("setUDP_listenPort"), QString("comm/udpListenPort"), "int");
-	addKey(QString("setUDP_sendPort"), QString("comm/udpSendPort"), "int");
+    addKey(QString("setUDP_listenPort"), QString("comm/udpListenPort"), "int");
+    addKey(QString("setUDP_sendPort"), QString("comm/udpSendPort"), "int");
 	addKey(QString("setUDP_listenOnly"), QString("comm/udpListenOnly"), "bool");
 	addKey(QString("setUDP_numConnections"), QString("comm/udpNumConnections"), "int");
-	addKey(QString("setUDP_IP1"), QString("comm/udpIP1"), "QString");
-	addKey(QString("setUDP_IP2"), QString("comm/udpIP2"), "QString");
-	addKey(QString("setUDP_IP3"), QString("comm/udpIP3"), "QString");
-	addKey(QString("setLoop_listenPort"), QString("comm/loopListenPort"), "int");
-	addKey(QString("setLoop_sendPort"), QString("comm/loopSendPort"), "int");
+    addKey(QString("setUDP_IP1"), QString("comm/udpIP1"), "int");
+    addKey(QString("setUDP_IP2"), QString("comm/udpIP2"), "int");
+    addKey(QString("setUDP_IP3"), QString("comm/udpIP3"), "int");
+    addKey(QString("setLoop_listenPort"), QString("comm/loopListenPort"), "int");
+    addKey(QString("setLoop_sendPort"), QString("comm/loopSendPort"), "int");
 	addKey(QString("setNTRIP_isOn"), QString("comm/ntripIsOn"), "bool");
 	addKey(QString("setRadio_isOn"), QString("comm/radioIsOn"), "bool");
 	addKey(QString("setSerialPass_isOn"), QString("comm/serialPassIsOn"), "bool");
-	addKey(QString("setNTRIP_casterPort"), QString("comm/ntripCasterPort"), "int");
+    addKey(QString("setNTRIP_casterPort"), QString("comm/ntripCasterPort"), "int");
 	addKey(QString("setNTRIP_mount"), QString("comm/ntripMount"), "QString");
 	addKey(QString("setNTRIP_userName"), QString("comm/ntripUserName"), "QString");
 	addKey(QString("setNTRIP_userPassword"), QString("comm/ntripUserPassword"), "QString");
@@ -24,7 +24,7 @@ void QMLSettings::setupKeys() {
 	addKey(QString("setNTRIP_manualLon"), QString("comm/ntripManualLon"), "double");
 	addKey(QString("setNTRIP_isTCP"), QString("comm/ntripIsTCP"), "bool");
 	addKey(QString("setNTRIP_isHTTP10"), QString("comm/ntripIsHTTP10"), "bool");
-	addKey(QString("setNTRIP_IP1"), QString("comm/ntripIP1"), "int");
-	addKey(QString("setNTRIP_IP2"), QString("comm/ntripIP2"), "int");
-	addKey(QString("setNTRIP_IP3"), QString("comm/ntripIP3"), "int");
+    addKey(QString("setNTRIP_IP1"), QString("comm/ntripIP1"), "int");
+    addKey(QString("setNTRIP_IP2"), QString("comm/ntripIP2"), "int");
+    addKey(QString("setNTRIP_IP3"), QString("comm/ntripIP3"), "int");
 }
